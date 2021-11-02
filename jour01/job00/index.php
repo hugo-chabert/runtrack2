002a7da91c035695887143fb4626a526e556e0e6
Hugo <hugo.chabert@laplateforme.io>
2021-11-02 10:14:47 +0100

    Creation job00 et job01

---
+++ jour01/job00/index.php
@@ -0,0 +1,3 @@
+<?php
+$str = "Hello LaPlateforme!";
+echo $str;
---
+++ jour01/job01/index.php
@@ -0,0 +1,16 @@
+<?php
+$str = "LaPlateforme ";
+echo $str;
+
+$str2 = "Vive ";
+$str3 = "!";
+echo $str2 ,$str ,$str3;
+
+$val = 6;
+echo $val;
+echo $val + 4;
+
+$myBool = true;
+echo $myBool;
+$myBool = false;
+echo $myBool;
