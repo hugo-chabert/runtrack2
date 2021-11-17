58cef93c1c3be68e494467f48a4c2bdac41966fc
Hugo <hugo.chabert@laplateforme.io>
2021-11-17 16:11:02 +0100

    HTML/CSS Connexion, Inscription et Accueil fini

--- jour06/job07/index.php
+++ jour06/job07/index.php
@@ -1,9 +1,23 @@
 <?php
-$tab = ;
+$tab = ["abc", "ghi", "def"];
+$croissant = true;
 
-function bubblesort($tab){
 
+function bubblesort($tab, $croissant){    
+    $alpha = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+    $i = 0;
+    if($croissant == true){
+        while(isset($croissant[$i])){
+            
+        }
+    }
+
+    else if($croissant != true){
+
+    }
 
 }
+
+echo $tab['0']['1'];
 
 ?>
